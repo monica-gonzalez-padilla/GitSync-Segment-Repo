71ecb1250682f71d145697db9f76889dbe88a554
Segment Git Sync <friends@segment.com>
2026-05-20 16:30:53 +0000

    Destination '6a0de1bb17dd02a0209ad7c0' created by monica.gzz95@gmail.com

---
+++ destinations/6a0de1bb17dd02a0209ad7c0.tf
@@ -0,0 +1,31 @@
+import {
+  to = segment_destination.id-6a0de1bb17dd02a0209ad7c0
+  id = "6a0de1bb17dd02a0209ad7c0"
+}
+
+resource "segment_destination" "id-6a0de1bb17dd02a0209ad7c0" {
+  enabled = false
+  metadata = {
+    contacts = [
+      {
+      },
+    ]
+    id                = "58f8f55a70a3e552b955a444"
+    partner_owned     = false
+    region_endpoints  = null
+    supported_regions = null
+  }
+  name = "HTTP API - Marketo"
+  settings = jsonencode({
+    accountId         = ""
+    anonymousActivity = false
+    beta              = false
+    clientId          = ""
+    clientSecret      = ""
+    events            = []
+    marketoFormId     = ""
+    marketoHostUrl    = ""
+    traits            = []
+  })
+  source_id = "tCbspwT9Y8vMy7E2tBBxnt"
+}
