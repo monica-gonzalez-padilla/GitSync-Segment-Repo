795e030f47ba23ec9cdff928ef5c9f7b2e395d4f
Segment Git Sync <friends@segment.com>
2026-09-17 19:53:11 +0000

    Source 'suvnvTBXKDSaTrBpNKUJEi' created by monica.gzz95@gmail.com

---
+++ sources/suvnvTBXKDSaTrBpNKUJEi.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-suvnvTBXKDSaTrBpNKUJEi
+  id = "suvnvTBXKDSaTrBpNKUJEi"
+}
+
+resource "segment_source" "id-suvnvTBXKDSaTrBpNKUJEi" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "kFoA4q45P3"
+  }
+  name     = "Wawa-SessionM Source Function"
+  settings = jsonencode({})
+  slug     = "wawa_sessionm_source_function_3_jtaz_fc_lw_k_oah5g2y5t_mm8s_fgl"
+}
